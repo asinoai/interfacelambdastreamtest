e98c99ab09289ea0474b8403358ed8eabeeb0a5f
aronsinoai <aron.sinoai@gmail.com>
2016-07-20 15:47:13 +0300

    New stream tests

--- main/src/test/java/aron/sinoai/interfacelambdastreamtest/StreamTest.java
+++ main/src/test/java/aron/sinoai/interfacelambdastreamtest/StreamTest.java
@@ -4,7 +4,10 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.atomic.DoubleAccumulator;
+import java.util.stream.Collectors;
 
 /**
  */
@@ -18,11 +21,44 @@ public class StreamTest
                 new Fruit("banana", 3.5)
         );
 
-        long count1 = fruitList.stream()
+        long count = fruitList.stream()
                 .filter((item) -> !item.getName().equals("apple"))
                 .count();
 
-        Assert.assertEquals("2 filtered items", 2l, count1);
+        Assert.assertEquals("2 filtered items", 2l, count);
+    }
+
+
+    @Test
+    public void sumTest() {
+        List<Fruit> fruitList = Arrays.asList(
+                new Fruit("apple", 1.0),
+                new Fruit("pear", 2.0),
+                new Fruit("banana", 3.5)
+        );
+
+        double sum = fruitList.stream()
+                .map(Fruit::getPrice)
+                .reduce(0.0, Double::sum);
+
+        Assert.assertEquals("Sum", 6.5, sum, 0);
+    }
+
+
+    @Test
+    public void collectTest() {
+        List<Fruit> fruitList = Arrays.asList(
+                new Fruit("apple", 1.0),
+                new Fruit("pear", 2.0),
+                new Fruit("banana", 3.5)
+        );
+
+        List<Fruit> sorted = fruitList.stream()
+                .sorted(Comparator.comparing(Fruit::getName))
+                .collect(Collectors.toList());
+
+        Assert.assertEquals("Pear is the last one", "pear", sorted.get(2).getName());
+
     }
 
 }
